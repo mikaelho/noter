f87d3e05adf139445a15b6e5f62a1d4a7e704d4b
mikaelho <mikael.honkala@gmail.com>
2017-10-13 12:51:34 +0300

    Delete noterconf.py

--- noterconf.py
+++
@@ -1,8 +0,0 @@
-#auth_token = "S=s48:U=4f38a3:E=16590e7fe5e:C=15e3936cf98:P=1cd:A=en-devtoken:V=2:H=c11d0a4f4128ae60ce48cca650870dd7"
-
-notebook_guid = '73f05389-6a3c-47c3-a56a-7c06e623fef8'
-
-reminder_namespace = 'Kult - Man and Dog'
-
-iphone = False
-
